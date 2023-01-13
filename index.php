376e0bb23a5bba03ad1db26c84c66467e6307585
Unravel-Pranav <pathekarpranav05@gmail.com>
2023-01-14 05:08:09 +0530

    first

--- index.php
+++ index.php
@@ -1,62 +1,256 @@
 <!DOCTYPE html>
 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Pranav Hotel</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="">
-        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-       <style>
-        *{
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Pranav's Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <style>
+        * {
             font-family: 'Poppins', sans-serif;
         }
-        .h-font{
+
+        .h-font {
             font-family: 'Merienda', cursive;
         }
-      </style>
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-        <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3  py-lg-2 shadow-sm sticky-top">
+        <div class="container-fluid px-lg-4 mt-4">
+            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Pranav's Hotel</a>
+            <button class="navbar-toggler  shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  me-2" aria-current="page" href="#">Rooms</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link me-2 " aria-current="page" href="#">Facilities</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  me-2" aria-current="page" href="#">Contact us</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " aria-current="page" href="#">About</a>
+                    </li>
+
+                </ul>
+                <div class="d-flex">
+
+                    <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+                        Login
+                    </button>
+                    <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">
+                        Register
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
     </nav>
 
+    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="">
+                    <div class="modal-header">
+                        <h5 class="modal-title d-flex align-items-center">
+                            <i class="bi bi-person-circle fs-3 me-2"></i>User Login
+                        </h5>
+                        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label class="form-label">Email address</label>
+                            <input type="email" class="form-control  shadow-none">
+                        </div>
 
-  
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
+                        <div class="mb-4">
+                            <label class="form-label">Password</label>
+                            <input type="password" class="form-control  shadow-none">
+                        </div>
+                        <div class="d-flex align-items-center justify-content-between mb-2">
+                            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+                            <a href="javascript: void(0) " class="text-secondary text-decoration-none">Forgot Password?</a>
+                        </div>
+                    </div>
+            </div>
+
+            </form>
+        </div>
+    </div>
+    </div>
+
+
+    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <form action="">
+                    <div class="modal-header">
+                        <h5 class="modal-title d-flex align-items-center">
+                            <i class="bi bi-person-lines-fill"></i>User Registeration
+                        </h5>
+                        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <span class="badge rounded-pill text-bg-light text-dark mb-3 text-wrap lh-base">
+                            Note: Your details must match your ID(Addhar Card,Passport,Driving Licence,etc..)
+                            That will be required during check-in.
+                        </span>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Name</label>
+                                    <input type="text" class="form-control  shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0">
+                                    <label class="form-label">Email</label>
+                                    <input type="email" class="form-control  shadow-none">
+                                </div>
+
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Phone Number</label>
+                                    <input type="number" class="form-control  shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0">
+                                    <label class="form-label">Picture</label>
+                                    <input type="file" class="form-control  shadow-none">
+                                </div>
+
+                                <div class="col-md-12 p-0 mb-3">
+                                    <label class="form-label">Address</label>
+                                    <div class="form-group">
+                                        <textarea class="form-control shadow-none" rows="1"></textarea>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Pincode</label>
+                                    <input type="number" class="form-control  shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0">
+                                    <label class="form-label">Date of birth</label>
+                                    <input type="date" class="form-control  shadow-none">
+                                </div>
+
+                                <div class="col-md-6 ps-0 mb-3">
+                                    <label class="form-label">Password</label>
+                                    <input type="password" class="form-control  shadow-none">
+                                </div>
+                                <div class="col-md-6 p-0">
+                                    <label class="form-label">Conform Password</label>
+                                    <input type="password" class="form-control  shadow-none">
+                                </div>
+                            </div>
+                            <div class="text-center my-1">
+                                <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+        <!-- <div class="mb-3">
+                                        <label class="form-label">Email address</label>
+                                        <input type="email" class="form-control  shadow-none">
+                                    </div>
+
+                                     <div class="mb-4">
+                                        <label class="form-label">Password</label>
+                                        <input type="password" class="form-control  shadow-none">
+                                    </div>  
+                                        <div class="d-flex align-items-center justify-content-between mb-2">
+                                          <button  type="submit" class="btn btn-dark shadow-none">LOGIN</button>
+                                          <a href="javascript: void(0) "class="text-secondary text-decoration-none">Forgot Password?</a>
+                                        </div> -->
+    </div>
+    </div>
+    </form>
+    </div>
+    </div>
+    </div>
+
+    <div class="container-fluid">
+        <!-- Swiper -->
+        <div class="swiper swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="images/carousel/1.png"class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/2.png" class="w-100 d-block"/>
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/3.png"class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/4.png"class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/5.png"class="w-100 d-block" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="images/carousel/6.png"class="w-100 d-block" />
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper(".swiper-container", {
+            spaceBetween: 30,
+            effect: "fade",
+            loop:true,
+            autoplay:{
+                delay: 3500,
+                disableOnInteraction:false,
+            }
+        });
+    </script>
+</body>
+
 </html>
