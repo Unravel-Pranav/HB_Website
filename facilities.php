14d02907cfc3d01dfb881b66295e0b7ff1e1a626
Unravel-Pranav <pathekarpranav05@gmail.com>
2023-01-29 00:19:36 +0530

    seventh

--- facilities.php
+++ facilities.php
@@ -2,32 +2,117 @@
 
 <html>
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Pranav's Hotel-Facilities</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Pranav's Hotel-Facilities</title>
 
-        <?php require('inc/links.php'); ?>
+    <?php require('inc/links.php'); ?>
+    <style>
+        .pop:hover{
+            border-top-color: var(--teal) !important;
+            transform: scale(1.03);
+            transition: all 0.3sec;
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+        }
+    </style>
 
-    </head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
 
-    <body class="bg-light">
-        <?php require('inc/header.php'); ?>
-        <div class="my-5 px-4">
+</head>
+
+<body class="bg-light">
+    <?php require('inc/header.php'); ?>
+    <div class="my-5 px-4">
 
         <h2 class="fw-bold h-font text-center ">OUR FACILITIES</h2>
-        <div class="h-line bg-dark"></div>
+        <div class="h-line  bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Unde veritatis deserunt earum <br>delectus error aspernatur.
+            Autem accusantium quisquam iste earum.
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> <img src="images/facilities/wifi.svg " width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe ducimus at iste quibusdam eos nam consequuntur!
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> <img src="images/facilities/wifi.svg " width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe ducimus at iste quibusdam eos nam consequuntur!
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> <img src="images/facilities/wifi.svg " width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe ducimus at iste quibusdam eos nam consequuntur!
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> <img src="images/facilities/wifi.svg " width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe ducimus at iste quibusdam eos nam consequuntur!
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> <img src="images/facilities/wifi.svg " width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe ducimus at iste quibusdam eos nam consequuntur!
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2"> <img src="images/facilities/wifi.svg " width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe ducimus at iste quibusdam eos nam consequuntur!
+                    </p>
+                </div>
+            </div>
+            
         </div>
+    </div>
 
 
 
-        <!-- Footer -->
+    <!-- Footer -->
 
-        <?php require('inc/footer.php'); ?>
+    <?php require('inc/footer.php'); ?>
 
 
-    </body>
+</body>
 
 </html>
