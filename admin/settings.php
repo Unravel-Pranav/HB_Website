72573eddd53675771ea45dde2b04466e89c6d84a
Unravel-Pranav <pathekarpranav05@gmail.com>
2023-07-29 01:41:34 +0530

    made contact in settings

--- admin/settings.php
+++ admin/settings.php
@@ -43,6 +43,7 @@ session_regenerate_id(true);
 
                     </div>
                 </div>
+
                 <!-- General Settings Modal -->
                 <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1"
                     aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -81,9 +82,7 @@ session_regenerate_id(true);
                     </div>
                 </div>
 
-
                 <!-- Shutdown Section -->
-
                 <div class="card border-0 shadow-sm">
                     <div class="card-body">
                         <div class="d-flex align-items-center justify-content-between mb-3">
@@ -102,113 +101,177 @@ session_regenerate_id(true);
 
                     </div>
                 </div>
-            </div>
 
+                <!-- Contact Details Section -->
+                <div class="card border-0 shadow-sm mb-4">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">Contacts Settings</h5>
+                            <button type="button" class="btn-dark shadow-none btn-sm" data-bs-toggle="modal"
+                                data-bs-target="#contacts-s">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </button>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">Address</h6>
+                                    <p class="card-text" id="address"></p>
+                                </div>
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">Google Map</h6>
+                                    <p class="card-text" id="gmap"></p>
+                                </div>
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">Phone Numbers</h6>
+                                    <p class="card-text mb-1" id="address"><i class="bi bi-telephone-fill"></i>
+                                        <span id="pn1"></span>
+                                    </p>
+                                    <p class="card-text" id="address"><i class="bi bi-telephone-fill"></i>
+                                        <span id="pn2"></span>
+                                    </p>
+                                </div>
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">E-mail</h6>
+                                    <p class="card-text" id="email"></p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="mb-4">
+                                    <h6 class="card-subtitle mb-1 fw-bold">Social Links</h6>
+                                    <p class="card-text mb-1" id="address"><i class="bi bi-facebook me-1"></i>
+                                        <span id="fb"></span>
+                                    </p>
+                                    <p class="card-text mb-1" id="address"><i class="bi bi-twitter me-1"></i>
+                                        <span id="tw"></span>
+                                    </p>
+                                    <p class="card-text" id="address"><i class="bi bi-instagram me-1"></i>
+                                        <span id="insta"></span>
+                                    </p>
+                                </div>
+                                <div class="mb-4">
+                                <h6 class="card-subtitle mb-1 fw-bold">iFrame</h6>
+                                <iframe id="iframe" class="border p-2 w-100" loading="lazy"></iframe>
+                            </div>
+                            </div>     
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <?php require('inc/scripts.php');  ?>
-        <script>
-        let general_data;
-        let general_s_form = document.getElementById('general_s_form');
 
-        let site_title_inp = document.getElementById('site_title_inp');
-        let site_about_inp = document.getElementById('site_about_inp');
 
-        function get_general() {
-            let site_title = document.getElementById('site_title');
-            let site_about = document.getElementById('site_about');
 
-            
 
-            let shutdown_toggle = document.getElementById('shutdown-toggle');
+    </div>
+    </div>
 
-            let xhr = new XMLHttpRequest();
+    <?php require('inc/scripts.php');  ?>
+    <script>
+    let general_data;
+    let general_s_form = document.getElementById('general_s_form');
 
-            xhr.open("POST", "ajax/settings_crud.php", true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    let site_title_inp = document.getElementById('site_title_inp');
+    let site_about_inp = document.getElementById('site_about_inp');
 
-            xhr.onload = function() {
-                general_data = JSON.parse(this.responseText);
+    function get_general() {
+        let site_title = document.getElementById('site_title');
+        let site_about = document.getElementById('site_about');
 
-                site_title.innerText = general_data.site_title;
-                site_about.innerText = general_data.site_about;
 
-                site_title_inp.value = general_data.site_title;
-                site_about_inp.value = general_data.site_about;
 
-                if (general_data.shutdown == 0) {
-                    shutdown_toggle.checked = false;
-                    shutdown_toggle.value = 0;
-                } else {
-                    shutdown_toggle.checked = true;
-                    shutdown_toggle.value = 1;
-                }
+        let shutdown_toggle = document.getElementById('shutdown-toggle');
 
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "ajax/settings_crud.php", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onload = function() {
+            general_data = JSON.parse(this.responseText);
+
+            site_title.innerText = general_data.site_title;
+            site_about.innerText = general_data.site_about;
+
+            site_title_inp.value = general_data.site_title;
+            site_about_inp.value = general_data.site_about;
+
+            if (general_data.shutdown == 0) {
+                shutdown_toggle.checked = false;
+                shutdown_toggle.value = 0;
+            } else {
+                shutdown_toggle.checked = true;
+                shutdown_toggle.value = 1;
             }
-
-            xhr.send('get_general');
-        }
-
-        general_s_form.addEventListener('submit',function(e)
-        {
-            e.preventDefault();
-            upd_general(site_title_inp.value, site_about_inp.value);
-
-        })
-
-        function upd_general(site_title_val, site_about_val) {
-            let xhr = new XMLHttpRequest();
-
-            xhr.open("POST", "ajax/settings_crud.php", true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-            xhr.onload = function() {
-                var myModal = document.getElementById('general-s');
-                var modal = bootstrap.Modal.getInstance(myModal);
-                modal.hide();
-
-
-                if (this.responseText == 1) {
-                    alert('success', 'Changes saved!')
-                    get_general();
-                } else {
-                    alert('error', 'No Changes saved!')
-                    console.log('no changes made')
-                }
-
-            }
-
-            xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&upd_general');
 
         }
 
-        function upd_shutdown(val) {
+        xhr.send('get_general');
+    }
 
-            let xhr = new XMLHttpRequest();
+    general_s_form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        upd_general(site_title_inp.value, site_about_inp.value);
 
-            xhr.open("POST", "ajax/settings_crud.php", true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    })
 
-            xhr.onload = function() {
-                if (this.responseText == 1 && general_data.shutdown == 0) {
+    function upd_general(site_title_val, site_about_val) {
+        let xhr = new XMLHttpRequest();
 
-                    alert('success', 'Site has been Shutdown!');
-                } else {
-                    alert('success', 'Shutdown mode off');
+        xhr.open("POST", "ajax/settings_crud.php", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-                }
+        xhr.onload = function() {
+            var myModal = document.getElementById('general-s');
+            var modal = bootstrap.Modal.getInstance(myModal);
+            modal.hide();
+
+
+            if (this.responseText == 1) {
+                alert('success', 'Changes saved!')
                 get_general();
-
+            } else {
+                alert('error', 'No Changes saved!')
+                console.log('no changes made')
             }
-
-            xhr.send('upd_shutdown=' + val);
 
         }
 
-        window.onload = function() {
+        xhr.send('site_title=' + site_title_val + '&site_about=' + site_about_val + '&upd_general');
+
+    }
+
+    function upd_shutdown(val) {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "ajax/settings_crud.php", true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onload = function() {
+            if (this.responseText == 1 && general_data.shutdown == 0) {
+
+                alert('success', 'Site has been Shutdown!');
+            } else {
+                alert('success', 'Shutdown mode off');
+
+            }
             get_general();
-        };
-        </script>
+
+        }
+
+        xhr.send('upd_shutdown=' + val);
+
+    }
+
+    window.onload = function() {
+        get_general();
+    };
+    </script>
 </body>
 
 </html>
